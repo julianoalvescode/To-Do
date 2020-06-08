1385f837550d26a2fb410611d7a8ae3e0bb2d55b
Juliano Alves <contato.julianoalvess@gmail.com>
2020-06-08 10:44:46 -0300

    feat: add favicon

--- src/page/Main/index.js
+++ src/page/Main/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TaskList from '../../components/TaskList';
 import Footer from '../../components/Footer';
 
 const Main = () => {
+    useEffect(() => {
+        console.log(
+            '%c Tentando revisar meu código né',
+            'font-size: 24px; font-weigth: bold;'
+        );
+        console.log(
+            '%c Então pegue esse presente aí 😎',
+            'font-size: 24px; font-weigth: bold;'
+        );
+        console.log(
+            '%c aHR0cHM6Ly93d3cudW1ibGVyLmNvbS9ici9zZWphLWJlbS12aW5kbz91PWtveGc3MDhr',
+            'font-size: 24px; font-weigth: bold;'
+        );
+    }, []);
+
     return (
         <>
             <TaskList />
